98837d4492e985f37d92895d45c288ed636c4831
Henrique Faria Brito Martini Correa <hcorrea@usf.edu>
2020-01-16 18:09:05 -0500

    commit change

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Visual_Studio2019_CSharpGitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
